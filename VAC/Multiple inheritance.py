0e63f08da1a97f2c00d1a121c25d9ce5bd947426
VISWANTH-89 <viswanth752@gmail.com>
2026-01-09 23:00:46 +0530

    Create Multiple inheritance.py

---
+++ VAC/Multiple inheritance.py
@@ -0,0 +1,12 @@
+#Multiple inheritance
+class A:
+  def feature1(self):
+    print("Feature 1")
+class B:
+  def feature2(self):
+    print("Feature 2")
+class C(A,B):
+  pass
+obj=C()
+obj.feature1()
+obj.feature2()
